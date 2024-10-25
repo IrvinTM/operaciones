d5e6342b0b63e3fac33851598b85b0ec7ab92036
IrvinTM <irvint698@gmail.com>
2024-10-25 10:19:19 -0600

    first commit

---
+++ src/main/java/com/irvin/Main.java
@@ -0,0 +1,8 @@
+package com.irvin;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+
+}
---
+++ src/main/java/com/irvin/Operaciones.java
@@ -0,0 +1,23 @@
+package com.irvin;
+
+public class Operaciones {
+
+    public static double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public static double restar(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
+}
